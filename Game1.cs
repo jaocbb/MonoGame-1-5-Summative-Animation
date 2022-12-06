a2dd1637eb252ccfb1a3fe589fb06cc09a86e8bd
BattJaco773 <jacobb8800@gmail.com>
2022-12-06 09:32:52 -0500

    Sounds

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,17 +7,21 @@ namespace MonoGame_1._5_Summative_Animation
 {
     public class Game1 : Game
     {
+       
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        Texture2D redCarTexture;
-        Rectangle redCarRect;
-        Texture2D copCarTexture;
-        Rectangle copCarRect;
-        KeyboardState keyboardState;
+       
+        Texture2D redCarTexture, copCarTexture;
+        Rectangle redCarRect, copCarRect;
+        KeyboardState KeyboardState;
+        KeyboardState State;
         Screen screen;
-        Texture2D introTexture;
-        Rectangle introRect;
-        SpriteFont font;
+        float seconds, startTime;
+        Texture2D introTexture,intro2Texture,OutroTexture;
+        Rectangle introRect,intro2Rect,OutroRect;
+        SpriteFont font,raceFont,timerFont,outroFont;
+        Vector2 copSpeed, redSpeed;
+        SoundEffect policeSiren,raceCar;
         enum Screen
         {
             Intro,
@@ -34,12 +39,18 @@ namespace MonoGame_1._5_Summative_Animation
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            screen = Screen.Intro;
             this.Window.Title = "MonoGame Pt1-5";
             _graphics.PreferredBackBufferHeight = 500;
             _graphics.PreferredBackBufferWidth = 800;
-            redCarRect = new Rectangle(50, 50, 500, 500);
-            copCarRect = new Rectangle(100, 100, 400, 500);
+            redCarRect = new Rectangle(40,370, 150, 50);
+            redSpeed = new Vector2(3, 3);
+            copCarRect = new Rectangle(0, 370, 90, 50);
+            copSpeed = new Vector2(2, 2);
             introRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight+50);
+            intro2Rect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight+20);
+            OutroRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight+20);
+            
             base.Initialize();
         }
 
@@ -49,19 +60,54 @@ namespace MonoGame_1._5_Summative_Animation
             redCarTexture = Content.Load<Texture2D>("redCar");
             copCarTexture = Content.Load<Texture2D>("copCar");
             introTexture = Content.Load<Texture2D>("Intro2");
+            intro2Texture = Content.Load<Texture2D>("BackgroundRace");
+            OutroTexture = Content.Load<Texture2D>("Outro");
             font = Content.Load<SpriteFont>("font");
+            raceFont = Content.Load<SpriteFont>("raceFont");
+            timerFont = Content.Load<SpriteFont>("timerFont");
+            outroFont = Content.Load<SpriteFont>("outroFont");
+            policeSiren = Content.Load<SoundEffect>("policeSiren");
+            raceCar= Content.Load<SoundEffect>("raceCar");
+
+
             // TODO: use this.Content to load your game content here 
         }
 
         protected override void Update(GameTime gameTime)
         {
+            seconds = (float)gameTime.TotalGameTime.TotalSeconds - startTime;
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
+            KeyboardState = Keyboard.GetState();
+            if (screen == Screen.Intro)
+            {
+                if (KeyboardState.IsKeyDown(Keys.Enter))
+                {
+                    screen = Screen.Race;
+                    policeSiren.Play();
+                    raceCar.Play();
+                }
+            }
+            else if (screen == Screen.Race)
+            {
+                redCarRect.X += (int)redSpeed.X;
+                if (redCarRect.X > _graphics.PreferredBackBufferWidth)
+                {
+                    screen=Screen.Outro;
+                    policeSiren.Dispose();
+                    raceCar.Dispose();  
+                }
+               
+                copCarRect.X += (int)copSpeed.X;
+                base.Update(gameTime);
+            }
+            
         }
+            
+               
+            
+         
+        
 
         protected override void Draw(GameTime gameTime)
         {
@@ -70,16 +116,23 @@ namespace MonoGame_1._5_Summative_Animation
             if (screen == Screen.Intro)
             {
                 _spriteBatch.Draw(introTexture, introRect, Color.White);
-                _spriteBatch.DrawString(font, "Click Enter to watch the great escape", new Vector2(250, 150), Color.White);
-                
+                _spriteBatch.DrawString(font, "The Great Escape", new Vector2(225, 30), Color.White);
+                _spriteBatch.DrawString(raceFont, "Click enter to begin ", new Vector2(300, 70), Color.White);
             }
             else if (screen == Screen.Race)
             {
+                _spriteBatch.Draw(intro2Texture, intro2Rect, Color.White);
                 _spriteBatch.Draw(redCarTexture, redCarRect, Color.White);
                 _spriteBatch.Draw(copCarTexture, copCarRect, Color.White);
+                _spriteBatch.DrawString(timerFont, (0 + seconds).ToString("00:00"), new Vector2(270, 100), Color.Black);
 
             }
-         
+            else if(screen == Screen.Outro) 
+            {
+                _spriteBatch.Draw(OutroTexture,OutroRect, Color.White);
+                _spriteBatch.DrawString(outroFont, "You've escaped your ticket! ", new Vector2(300, 70), Color.Black);
+            }
+
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
